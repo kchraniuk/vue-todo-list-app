276d4ea8f6c43820758c47630b74c7ec60f31d14
Kamil Chraniuk <kchra@softserveinc.com>
2018-04-05 15:38:17 +0200

    added project files

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <!-- <search :tasks="tasks" @searchChange="searchValueChanged" /> -->
+    <search :tasks="tasks" />
+    <newTask :tasks="tasks" />
+    <div></div>
+  </div>
+</template>
+
+<script>
+  import search from '@/components/search'
+  import newTask from '@/components/addNewTask'
+
+  export default {
+    name: 'App',
+    data: function () {
+      return {
+        tasks: [
+          { description: 'task 1', status: true },
+          { description: 'task 2', status: true },
+          { description: 'task 3', status: false },
+          { description: 'task 4', status: false }
+        ]
+      }
+    },
+    components: {
+      search,
+      newTask
+    }
+  }
+</script>
+
+<style lang="sass">
+  @import "stylesheets/app.scss"
+</style>
---
+++ src/components/addNewTask.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="newTask">
+    <form @submit.prevent="addNewTask(newTaskModel)">
+        <input type="text" v-model="newTaskModel" :placeholder="newTaskPlaceholder" />
+        <button :disabled="newTaskModel.length < 1" type="submit">Add new</button>
+    </form>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'newTask',
+    methods: {
+      addNewTask (task){
+
+        if(task.length > 1) {
+          this.tasks.push({
+            description: task,
+            status: false
+          })
+        }
+      }
+    },
+    data() {
+      return {
+        newTaskModel: '',
+        newTaskPlaceholder: 'Enter the task name'
+      }
+    },
+    props: {
+      tasks: {required: true}
+    }
+  }
+</script>
+
+<style lang="sass">
+  @import '../stylesheets/components/addNewTask.scss'
+</style>
+
